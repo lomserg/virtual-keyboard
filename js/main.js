60d8a9c3f22816966f6458c1d03b61b12c78f90d
lomserg <lomserg@gmail.com>
2023-05-01 19:37:37 +0300

    refactor: apply eslint

--- js/main.js
+++ js/main.js
@@ -1,36 +1,32 @@
+import Keyboard from './modules/keyboard.js';
+import keys from './modules/keyslayout.js';
 
-import Keyboard from './modules/keyboard.js'
-import keys from './modules/keyslayout.js'
+const newKeyboard = new Keyboard(keys);
 
-
-const newKeyboard = new Keyboard(keys)
-
-
-newKeyboard.init()
+newKeyboard.init();
 
 // helper event hendle functions
 function eventHandler(e) {
-    e.preventDefault()
-    const keyboardKey = e.code
-    const keyDetail = newKeyboard.getKeyDetail(keyboardKey)
-    if (keyDetail) {
-        newKeyboard.utils(e.code, e.type);
-        if (e.type === 'keydown') {
-            newKeyboard.keyType(keyDetail);
-        }
-      }
+  e.preventDefault();
+  const keyboardKey = e.code;
+  const keyDetail = newKeyboard.getKeyDetail(keyboardKey);
+  if (keyDetail) {
+    newKeyboard.utils(e.code, e.type);
+    if (e.type === 'keydown') {
+      newKeyboard.keyType(keyDetail);
+    }
   }
+}
 
-  document.addEventListener("keydown", eventHandler)
-  document.addEventListener("keyup", eventHandler)
-  document.querySelector('textarea').focus()
-  document.querySelector('.keyboard').addEventListener('mousedown', (e)=> {
-    
-    const keyboardKey = e.target.dataset.keyCode
-    const keyDetail = newKeyboard.getKeyDetail(keyboardKey)
-    newKeyboard.keyType(keyDetail);
-    newKeyboard.utils(keyboardKey, e.type);
-  })
+document.addEventListener('keydown', eventHandler);
+document.addEventListener('keyup', eventHandler);
+document.querySelector('textarea').focus();
+document.querySelector('.keyboard').addEventListener('mousedown', (e) => {
+  const keyboardKey = e.target.dataset.keyCode;
+  const keyDetail = newKeyboard.getKeyDetail(keyboardKey);
+  newKeyboard.keyType(keyDetail);
+  newKeyboard.utils(keyboardKey, e.type);
+});
 // document.addEventListener("keydown", e => {
 //     e.preventDefault();
 //     const keyboardKey = e.code
@@ -44,9 +40,9 @@ function eventHandler(e) {
 //         }
 //     } else if(e.shiftKey && e.repeat) {
 //         console.log('shift down')
- 
+
 //     } else if(!e.shiftKey) {
-        
+
 //     }
 
 //   })
